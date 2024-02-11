bce4f830121e9237f7bd9f75007057ee360fe7a0
Talmaciu Elena <148704214+ellat99@users.noreply.github.com>
2024-02-11 18:01:38 +0200

    gbdf

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
+import { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { ShareLayout } from './SearchInput/SearchInput';
+const Trending = lazy(() => import('../pages/Trending/Trending'));
+const Movies = lazy(() => import('../pages/Movies/Movies'));
+const MoviesDetails = lazy(() =>
+  import('../pages/MoviesDetails/MoviesDetails')
+);
+const Cast = lazy(() => import('../components/Cast/Cast'));
+const Reviews = lazy(() => import('../components/Reviews/Reviews'));
+const NotFound = lazy(() => import('../pages/NotFound/NotFound'));
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  <Routes>
+    <Route path="/" element={<ShareLayout />} />
+    <Route index element={<Trending />} />
+    <Route path="movies" element={<Movies />} />
+    <Route path="movies/:id" element={<MoviesDetails />} />
+
+    <Route path="cast" element={<Cast />} />
+    <Route path="reviews" element={<Reviews />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>;
 };
---
+++ src/pages/Movies/Movies.jsx
@@ -0,0 +1,40 @@
+import { SearchInput } from 'components/SearchInput/SearchInput';
+import { ShowMoviesList } from '../ShowMoviesList/ShowMoviesList';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { searchMovies } from 'services/API';
+
+const Movies = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [moviesList, setMoviesList] = useState([]);
+  const query = searchParams.get('query') ?? '';
+
+  useEffect(() => {
+    const asyncFunc = async () => {
+      try {
+        //searchMovies(query) pentru a obține lista de filme corespunzătoare căutării.
+        setMoviesList(await searchMovies(query));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    asyncFunc();
+  }, [query]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    //Valoarea din câmpul de căutare este extrasă și setată ca parametru de căutare folosind setSearchParams({ query: form.elements.query.value }). Astfel, URL-ul se actualizează cu noul parametru de căutare.
+    setSearchParams({ query: form.elements.query.value });
+    form.reset();
+  };
+
+  return (
+    <>
+      <SearchInput onSubmit={handleSubmit} />
+      <ShowMoviesList movies={moviesList} navigate={''} />
+    </>
+  );
+};
+
+export default Movies;
